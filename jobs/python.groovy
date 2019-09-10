94d8ecf48b9d9ca6e4d85777d66b1f2ce36ed774
jugger25 <danijugger25@gmail.com>
2019-09-10 10:24:32 -0400

    Añadimos creacion de carpeta

--- jobs/python.groovy
+++ jobs/python.groovy
@@ -2,6 +2,11 @@ project_name = "jenkins-pipeline-python"
 repo = "git@github.com:Jugger25/tests-jenkins-1.git"
 repo_name = "repo"
 
+folder('proyect-a'){
+  displayName('Proyect A')
+  description('Folder for project A')
+}
+
 pipelineJob(project_name){
   definition{
     triggers{
